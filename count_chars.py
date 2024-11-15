0e8bc7c352cba8d6be9fd73e3f9bf90422effa1a
DinisB <dinisbarroso.dev@gmail.com>
2024-11-15 10:42:07 +0000

    Remove espaços

--- count_chars.py
+++ count_chars.py
@@ -2,10 +2,13 @@ frase = input()
 
 letras = {}
 
+
 for letter in frase:
     if letter in letras:
         letras[letter] += 1
     else:
         letras[letter] = 1
+
+letras.pop(' ')
 
 print(letras)
