0d4b88bc28331ffab5a5afb7d5ce69c9e2044dc5
DinisB <dinisbarroso.dev@gmail.com>
2024-11-15 11:37:03 +0000

    Update

--- char_intersection.py
+++ char_intersection.py
@@ -3,13 +3,13 @@ frase2 = input()
 words = ""
 list_palavra1 = frase1.split(" ")
 list_palavra2 = frase2.split(" ")
-set = set()
+list = set()
 
 for i in list_palavra1:
     if i in list_palavra2:
-        set.add(i)
+        list.add(i)
 
-for i in set:
+for i in list:
     words = words + " " +(i)
 
 print(words.replace(" ", "", 1))
